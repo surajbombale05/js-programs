f75843394f524de2b0c5e9d831f7bd4ce9d4f1d9
surajbombale05 <babanbombale59@gmail.com>
2024-07-15 16:05:06 +0530

    chagnes

---
+++ first.js
@@ -0,0 +1,155 @@
+//1  find the biggest number of elements form the array
+
+// let arr = [23,45,6,7,8,9,10,11,12,13];
+
+// let max = 0;
+
+// for (var i = 0; i < arr.length; i++){
+//     if (arr[i] > max  ){
+//         max = arr[i];
+//     }
+// }
+
+
+// console.log(max);
+
+// 2  finding the command prefix from the list of the arrays 
+
+//  let arr = ["suraj" , "samir" ," sarthak"];
+//  let LongestPre;
+
+//  let sorted =arr.sort();
+//  console.log(sorted);
+
+//  for (var i = 0 ; i < arr[0].length; i++){
+//      const char = arr[0][i];
+//      for (var j = 1; j < arr.length; j++){
+//          if (arr[j])
+//      }
+//  }
+
+//3 find the average of the array,
+
+// let arr = [2, 4 , 6 , 8];
+// let average =0;
+// let add =0;
+
+// for (let i = 0; i< arr.length ; i++){
+//     add = add+ arr[i];
+// }
+
+// console.log(add);
+
+// average = add / arr.length;
+// console.log(average);
+
+
+// 4  check how many vowels are present in the array
+// let vowels = "suraj";
+
+// let count = 0;
+
+// for (let i = 0; i < vowels.length; i++){
+//     if (vowels[i] === "a" || vowels[i] === "e" || vowels[i] === "i" || vowels[i] === "o" || vowels[i] === "u"){
+//         count++;
+//     }
+
+
+// }
+
+// console.log(count);
+
+
+// 5 write a function that return longest word form a sentence
+
+//  let sentence = "Hello World How are you today";
+//  let longest = "";
+//  let words = sentence.split(" ");
+//   for (var i = 0 ; i< words.length; i++){
+//     if (words[i].length > longest.length){
+//       longest = words[i];
+//     }
+//   }
+//  console.log(longest);
+
+// 6 palindrome numebers 
+
+// let newname = "suraj";
+
+// console.log(palindrome(newname)); // true
+
+// function palindrome(newname) {
+  
+// for(let i = 0; i < newname.length/2 ;i++){
+//   if (newname[i] === newname[newname.length-1-i]){
+//      return true;
+//   }
+//   return false;
+// }
+// }
+
+//7 write a number that checks given number prive or not 
+
+// let number = 7;
+
+// console.log(isPrime(number));
+
+// function isPrime(number) {
+//   if (number <= 1) {
+//     return false;    
+//   }
+//   for (let i = 1; i< number; i++ ){
+//      if (number %2 === 0){
+//       return false;
+//      }
+//   }
+//   return true;
+// }
+
+// 8
+// let arr = [1, 2, 3, 4, 5, 6, 7, 6];
+// let arr2 =[];
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = i+1; j < arr.length; j++) {
+//         if (arr[i] === arr[j]) {
+//            arr2.push(arr[i])
+
+//         }
+
+//     }
+// }
+
+// for (let k = 0; k < arr2.length; k++) {
+//    console.log(arr2[k]);
+// }
+
+// 9 write code to perform basic arithmetic operations 
+
+// its to easy to write
+
+// 10
+
+// let name1 = "rohit";
+// var a =name1.reverse();
+
+// console.log(a);
+
+
+// let original = "Hello World";
+// let reversed = original.split('').reverse().join('');
+// console.log(reversed);
+
+// let arr = [];
+
+
+// for (let i = 0; i < original.length; i++) {
+//     arr.push(original[i]);
+// }
+
+// for(let i = arr.length-1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
+
+
+
